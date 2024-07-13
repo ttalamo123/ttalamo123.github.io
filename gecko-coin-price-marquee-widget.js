9b8f7dfdb5f1f80264cc0def3e19eeaa462c2f57
ttalamo123 <39605816+ttalamo123@users.noreply.github.com>
2024-07-13 14:01:27 -0500

    Create gecko-coin-price-marquee-widget.js

---
+++ gecko-coin-price-marquee-widget.js
@@ -0,0 +1,2 @@
+<script src="https://widgets.coingecko.com/gecko-coin-price-marquee-widget.js"></script>
+<gecko-coin-price-marquee-widget locale="en" transparent-background="true" coin-ids="bitcoin,ripple,ethereum,cardano,solana,avalanche-2,polkadot,kucoin-shares,binancecoin,shiba-inu,dogecoin,terra-luna" initial-currency="usd"></gecko-coin-price-marquee-widget>
